8d8a20461f007d558438fdfc16ecba4395453244
Manuel Peracci (Fornitori Gruppo ISP) <m.peracci@reply.it>
2024-04-02 15:45:46 +0200

    again CRUD_API

---
+++ 04-REST_CRUD_API/03-CRUD_generic_API/src/main/java/com/CRUD_REST_API/demo/service/EmployeeServiceImpl.java
@@ -0,0 +1,53 @@
+package com.CRUD_REST_API.demo.service;
+
+import com.CRUD_REST_API.demo.dao.EmployeeDAO;
+import com.CRUD_REST_API.demo.dao.EmployeeRepository;
+import com.CRUD_REST_API.demo.entity.Employee;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.lang.RuntimeException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository){
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        Optional<Employee> result = employeeRepository.findById(theId);
+
+        Employee theEmployee = null;
+        if (result.isPresent()){
+            theEmployee = result.get();
+        } else {
+            throw new RuntimeException("Did not find employee id - " + theId);
+        }
+
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeRepository.save(theEmployee);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        employeeRepository.deleteById(theId);
+    }
+}
