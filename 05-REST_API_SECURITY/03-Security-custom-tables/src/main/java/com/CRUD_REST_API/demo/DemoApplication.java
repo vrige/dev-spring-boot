7545ae9ffea804fdebc9b103028124d317e89e68
Manuel Peracci (Fornitori Gruppo ISP) <m.peracci@reply.it>
2024-04-03 10:09:36 +0200

    final project for the security chapter

---
+++ 05-REST_API_SECURITY/03-Security-custom-tables/src/main/java/com/CRUD_REST_API/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.CRUD_REST_API.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
