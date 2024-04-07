a91d853c46c5d18a885357ac48d48c23baae695c
vrige <manuel.96524@gmail.com>
2024-04-07 13:29:37 +0200

    different types of inputs (checkboxes, etc.)

--- 06-MVC/01-MVC_thymeleaf_demo/src/main/java/com/sixth_chapter/thymeleaf_demo/controller/StudentController.java
+++ 06-MVC/01-MVC_thymeleaf_demo/src/main/java/com/sixth_chapter/thymeleaf_demo/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.sixth_chapter.thymeleaf_demo.controller;
 
 import com.sixth_chapter.thymeleaf_demo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +9,20 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${languages}")
+    private List<String> languages;
+
+    @Value("${systems}")
+    private List<String> systems;
+
 
     @RequestMapping("/showStudentForm")
     public String showForm(Model theModel){
@@ -17,6 +30,12 @@ public class StudentController {
         Student theStudent = new Student();
 
         theModel.addAttribute("student",theStudent);
+
+        theModel.addAttribute("countries",countries);
+
+        theModel.addAttribute("languages",languages);
+
+        theModel.addAttribute("systems",systems);
 
         return "student-form";
     }
--- 06-MVC/01-MVC_thymeleaf_demo/src/main/java/com/sixth_chapter/thymeleaf_demo/model/Student.java
+++ 06-MVC/01-MVC_thymeleaf_demo/src/main/java/com/sixth_chapter/thymeleaf_demo/model/Student.java
@@ -1,11 +1,15 @@
 package com.sixth_chapter.thymeleaf_demo.model;
 
+import java.util.List;
+
 public class Student {
 
     private String firstName;
     private String lastName;
 
+    private String favoriteLanguage;
     private String country;
+    private List<String> favoriteSystems;
 
     public Student(){
 
@@ -19,12 +23,28 @@ public class Student {
         this.firstName = firstName;
     }
 
+    public List<String> getFavoriteSystems() {
+        return favoriteSystems;
+    }
+
+    public void setFavoriteSystems(List<String> favoriteSystems) {
+        this.favoriteSystems = favoriteSystems;
+    }
+
     public String getLastName() {
         return lastName;
     }
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
     }
 
     public String getCountry() {
