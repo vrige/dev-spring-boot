7983de874dd9a47754b92b924c1eaa8f4bac54c6
U0M8235 <aysa.javadzad@esternibisp.com>
2024-04-18 12:57:49 +0200

    Starting chapter 8

---
+++ 08-MVC - SECURITY_/01-MVC-security/src/main/java/com/chapter_8/demo_security/DemoSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.chapter_8.demo_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSecurityApplication.class, args);
+	}
+
+}
---
+++ 08-MVC - SECURITY_/01-MVC-security/src/test/java/com/chapter_8/demo_security/DemoSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chapter_8.demo_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
